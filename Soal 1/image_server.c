1c20be567e0cfea9406a19e38c7fe93c88ba8870
Benjamin Skibidi <nathaillah29@gmail.com>
2025-05-08 15:24:58 +0700

    image_server.c

--- Soal 1/image_server.c
+++ Soal 1/image_server.c
@@ -1,167 +1,180 @@
 /* image_server.c - Program Sisop moudl 3 anjasssssssss walawe aku mumet */
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <pthread.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
+#include <sys/types.h>
+#include <unistd.h>
 #include <fcntl.h>
 #include <time.h>
-#include <sys/stat.h>
+#include <errno.h>
 
-#define PORT 8080
-#define MAX_CLIENTS 10
-#define BUFFER_SIZE 2048
-#define DB_FOLDER "server/database/"
-#define LOG_FILE "server/server.log"
+#define LISTEN_PORT 9090
+#define MAX_BUFFER 8192
+#define STORAGE_DIR "./data_store/"
 
 
-// fungsi nyatet semua aksi2 mencurigakan wakak
-void tulisLog(const char *asal, const char *aksi, const char *info) {
-    FILE *logFile = fopen(LOG_FILE, "a");
-    if (!logFile) return;
+void handle_connection(int client_fd);
+void process_decryption(int client_fd, const char *hex_input);
+void process_download(int client_fd, const char *file_name);
+int convert_hex_to_bin(const char *hex_str, unsigned char **bin_data);
 
-    time_t skrg = time(NULL);
-    struct tm *t = localtime(&skrg);
-    char waktu[32];
-    strftime(waktu, sizeof(waktu), "%Y-%m-%d %H:%M:%S", t);
+void handle_connection(int client_fd) {
+    char buffer[MAX_BUFFER];
+    ssize_t bytes_received;
 
-    fprintf(logFile, "[%s][%s]: [%s] [%s]\n", asal, waktu, aksi, info);
-    fclose(logFile);
-}
+    while ((bytes_received = recv(client_fd, buffer, sizeof(buffer) - 1, 0)) > 0) {
+        buffer[bytes_received] = '\0';
+        char *command = strtok(buffer, "::");
+        char *argument = strtok(NULL, "::");
 
-// reverse string
-void balikString(char *str) {
-    int pnjg = strlen(str);
-    for (int i = 0; i < pnjg / 2; i++) {
-        char tmp = str[i];
-        str[i] = str[pnjg - 1 - i];
-        str[pnjg - 1 - i] = tmp;
-    }
-}
-
-// hex to bytes converter
-int hexKeByte(const char *hex, unsigned char *hasil) {
-    int pnjg = strlen(hex);
-    int jml = 0;
-    for (int i = 0; i < pnjg; i += 2) {
-        if (i + 1 >= pnjg) break;
-        sscanf(hex + i, "%2hhx", &hasil[jml]);
-        jml++;
-    }
-    return jml;
-}
-
-// thread client, yg hold tiap client yg connect 
-void *handleClient(void *arg) {
-    int sockClient = *(int *)arg;
-    free(arg);
-
-    char pesanDariClient[BUFFER_SIZE] = {0};
-    recv(sockClient, pesanDariClient, sizeof(pesanDariClient), 0);
-
-    char *perintah = strtok(pesanDariClient, "|");
-    char *isiData = strtok(NULL, "");
-
-    if (!perintah || !isiData) {
-        send(sockClient, "Invalid command cuy\n", 21, 0);
-        close(sockClient);
-        return NULL;
-    }
-
-    if (strcmp(perintah, "DECRYPT") == 0) {
-        tulisLog("Client", "DECRYPT", "data teks aneh");
-
-        balikString(isiData);
-        unsigned char hasilDecode[BUFFER_SIZE] = {0};
-        int panjangData = hexKeByte(isiData, hasilDecode);
-
-        time_t skrg = time(NULL);
-        char namaFile[64];
-        snprintf(namaFile, sizeof(namaFile), "%s%ld.jpeg", DB_FOLDER, skrg);
-
-        FILE *fileKeluar = fopen(namaFile, "wb");
-        if (fileKeluar) {
-            fwrite(hasilDecode, 1, panjangData, fileKeluar);
-            fclose(fileKeluar);
-            tulisLog("Server", "SAVE", strrchr(namaFile, '/') + 1);
-            send(sockClient, strrchr(namaFile, '/') + 1, strlen(strrchr(namaFile, '/') + 1), 0);
+        if (command && argument) {
+            if (strcmp(command, "DECRYPT") == 0) {
+                process_decryption(client_fd, argument);
+            } else if (strcmp(command, "DOWNLOAD") == 0) {
+                process_download(client_fd, argument);
+            } else {
+                const char *msg = "Invalid command\n";
+                send(client_fd, msg, strlen(msg), 0);
+            }
         } else {
-            send(sockClient, "Gagal nyimpen file bro\n", 25, 0);
+            const char *msg = "Malformed request\n";
+            send(client_fd, msg, strlen(msg), 0);
         }
+    }
+}
 
-    } else if (strcmp(perintah, "DOWNLOAD") == 0) {
-        tulisLog("Client", "DOWNLOAD", isiData);
+void process_decryption(int client_fd, const char *hex_input) {
+    unsigned char *binary_data = NULL;
+    int binary_length = convert_hex_to_bin(hex_input, &binary_data);
 
-        char pathFile[128];
-        snprintf(pathFile, sizeof(pathFile), "%s%s", DB_FOLDER, isiData);
-        FILE *file = fopen(pathFile, "rb");
-
-        if (!file) {
-            send(sockClient, "File kaga ada cuy\n", 19, 0);
-            tulisLog("Server", "ERROR", "File kaga ketemu");
-        } else {
-            unsigned char kontenFile[BUFFER_SIZE] = {0};
-            int dibaca = fread(kontenFile, 1, sizeof(kontenFile), file);
-            fclose(file);
-            send(sockClient, kontenFile, dibaca, 0);
-            tulisLog("Server", "UPLOAD", isiData);
-        }
-    } else {
-        send(sockClient, "Perintah ga jelas sob\n", 23, 0);
+    if (binary_length <= 0) {
+        const char *msg = "Decryption failed\n";
+        send(client_fd, msg, strlen(msg), 0);
+        return;
     }
 
-    close(sockClient);
-    return NULL;
+    time_t current_time = time(NULL);
+    char file_path[256];
+    snprintf(file_path, sizeof(file_path), "%s%ld_output.jpeg", STORAGE_DIR, current_time);
+
+    int file_fd = open(file_path, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+    if (file_fd < 0) {
+        perror("File creation failed");
+        const char *msg = "File saving failed\n";
+        send(client_fd, msg, strlen(msg), 0);
+        free(binary_data);
+        return;
+    }
+
+    write(file_fd, binary_data, binary_length);
+    close(file_fd);
+    free(binary_data);
+
+    const char *msg = "Decryption and saving successful\n";
+    send(client_fd, msg, strlen(msg), 0);
 }
 
-// penDaemon
-void jadiDaemon() {
-    pid_t pid = fork();
-    if (pid < 0) exit(1);
-    if (pid > 0) exit(0);
+void process_download(int client_fd, const char *file_name) {
+    char file_path[256];
+    snprintf(file_path, sizeof(file_path), "%s%s", STORAGE_DIR, file_name);
 
-    umask(0);
-    setsid();
-    // chdir("/") tadinya ini dipake...ta ubah jadi komentar biar linenya gakepake..soalnya gaperlu keluar directory kan ya?
-    close(STDIN_FILENO);
-    close(STDOUT_FILENO);
-    close(STDERR_FILENO);
+    int file_fd = open(file_path, O_RDONLY);
+    if (file_fd < 0) {
+        const char *msg = "Requested file not found\n";
+        send(client_fd, msg, strlen(msg), 0);
+        return;
+    }
+
+    char buffer[MAX_BUFFER];
+    ssize_t bytes_read;
+    while ((bytes_read = read(file_fd, buffer, sizeof(buffer))) > 0) {
+        send(client_fd, buffer, bytes_read, 0);
+    }
+
+    close(file_fd);
 }
 
-//main function
+int convert_hex_to_bin(const char *hex_str, unsigned char **bin_data) {
+    size_t hex_len = strlen(hex_str);
+    if (hex_len % 2 != 0) return -1;
+
+    size_t bin_len = hex_len / 2;
+    *bin_data = malloc(bin_len);
+    if (!*bin_data) return -1;
+
+    for (size_t i = 0; i < bin_len; ++i) {
+        if (sscanf(hex_str + 2 * i, "%2hhx", &((*bin_data)[i])) != 1) {
+            free(*bin_data);
+            return -1;
+        }
+    }
+
+    return bin_len;
+}
+
 int main() {
-    mkdir("server/database", 0755); // in case gada foldernya..bikin dulu
+    int server_fd, client_fd;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t client_len = sizeof(client_addr);
 
-    jadiDaemon(); //daemon aktivator
+    signal(SIGCHLD, SIG_IGN);  // Ignore SIGCHLD to prevent zombie processes
 
-    int fdServer, sockBaru;
-    struct sockaddr_in alamat;
-    socklen_t panjangAlamat = sizeof(alamat);
-    int opsi = 1;
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);  // Create server socket
+    if (server_fd < 0) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
 
-    fdServer = socket(AF_INET, SOCK_STREAM, 0);
-    setsockopt(fdServer, SOL_SOCKET, SO_REUSEADDR, &opsi, sizeof(opsi));
+    // Set socket options for address reuse
+    int opt = 1;
+    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
-    alamat.sin_family = AF_INET;
-    alamat.sin_addr.s_addr = INADDR_ANY;
-    alamat.sin_port = htons(PORT);
+    // Bind to the specified port
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(LISTEN_PORT);
 
-    bind(fdServer, (struct sockaddr *)&alamat, sizeof(alamat));
-    listen(fdServer, MAX_CLIENTS);
+    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Binding failed");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    // Start listening for incoming connections
+    if (listen(server_fd, 10) < 0) {
+        perror("Listening failed");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Server is operational on port %d\n", LISTEN_PORT);
 
     while (1) {
-        sockBaru = accept(fdServer, (struct sockaddr *)&alamat, &panjangAlamat);
-        if (sockBaru >= 0) {
-            pthread_t benang;
-            int *pSock = malloc(sizeof(int));
-            *pSock = sockBaru;
-            pthread_create(&benang, NULL, handleClient, pSock);
-            pthread_detach(benang);
+        client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_len);
+        if (client_fd < 0) {
+            perror("Accept failed");
+            continue;
+        }
+
+        pid_t pid = fork();
+        if (pid == 0) {
+            close(server_fd);
+            handle_connection(client_fd);
+            close(client_fd);
+            exit(0);
+        } else if (pid > 0) {
+            close(client_fd);
+        } else {
+            perror("Fork failed");
+            close(client_fd);
         }
     }
 
+    close(server_fd);
     return 0;
 }
